39f49f582b67cf47b8c1cacb149ca540850a740a
Gejun Zhu <gejun.anderson@gmail.com>
2021-09-04 15:18:11 -0500

    updated train

--- train.py
+++ train.py
@@ -21,41 +21,44 @@ CAT_FEATURES = [
     "native-country",
 ]
 
+def go():
+    df = pd.read_csv("data/census_cleaned.csv")
 
-df = pd.read_csv("data/census_cleaned.csv")
+    df_train, df_test = train_test_split(df, test_size=0.2, random_state=42)
 
-df_train, df_test = train_test_split(df, test_size=0.2, random_state=42)
+    logging.info(f'Training sample size: {len(df_train)}')
+    logging.info(f'Testing sample size: {len(df_test)}')
 
-logging.info(f'Training sample size: {len(df_train)}')
-logging.info(f'Testing sample size: {len(df_test)}')
+    X_train, y_train, encoder, lb = process_data(df_train, CAT_FEATURES, 'salary')
+    save_to_file(encoder, f'model/encoder.pkl')
+    save_to_file(lb, f'model/lb.pkl')
 
-X_train, y_train, encoder, lb = process_data(df_train, CAT_FEATURES, 'salary')
-save_to_file(encoder, f'model/encoder.pkl')
-save_to_file(lb, f'model/lb.pkl')
+    X_test, y_test, _, _ = process_data(
+        df_test, CAT_FEATURES, 'salary', False, encoder, lb)
 
-X_test, y_test, _, _ = process_data(
-    df_test, CAT_FEATURES, 'salary', False, encoder, lb)
+    lr = train(X_train, y_train)
+    save_to_file(lr, f'model/lr.pkl')
 
-lr = train(X_train, y_train)
-save_to_file(lr, f'model/lr.pkl')
+    y_pred = inference(lr, X_test)
 
-y_pred = inference(lr, X_test)
-
-precision, recall, fbeta = compute_model_metrics(y_test, y_pred)
-logging.info('Metrics on Testing data:')
-logging.info(f'Precision: {precision}, Recall: {recall}, fbeta: {fbeta}')
+    precision, recall, fbeta = compute_model_metrics(y_test, y_pred)
+    logging.info('Metrics on Testing data:')
+    logging.info(f'Precision: {precision}, Recall: {recall}, fbeta: {fbeta}')
 
 
-for feature in CAT_FEATURES:
+    for feature in CAT_FEATURES:
 
-    data = pd.DataFrame({feature: df_test[feature],
-                         'salary': y_test,
-                         'pred': y_pred})
-    with open(f'model/slice_output.txt', 'a') as f:
-        f.write(f'----------- {feature} ----------\n')
+        data = pd.DataFrame({feature: df_test[feature],
+                            'salary': y_test,
+                            'pred': y_pred})
+        with open(f'model/slice_output.txt', 'a') as f:
+            f.write(f'----------- {feature} ----------\n')
 
-        for val in data[feature].unique():
-            tmp_df = data[data[feature] == val]
-            precision, recall, fbeta = compute_model_metrics(
-                tmp_df['salary'], tmp_df['pred'])
-            f.write(f'{val} - Precision: {str(round(precision, 2))}, Recall: {str(round(recall,2))}, fbeta: {str(round(fbeta, 2))}.\n')
+            for val in data[feature].unique():
+                tmp_df = data[data[feature] == val]
+                precision, recall, fbeta = compute_model_metrics(
+                    tmp_df['salary'], tmp_df['pred'])
+                f.write(f'{val} - Precision: {str(round(precision, 2))}, Recall: {str(round(recall,2))}, fbeta: {str(round(fbeta, 2))}.\n')
+
+if __name__ == '__main__':
+    go()
